759f7b4ce664ca3ed0915c3ccd196ce7cfcebaa9
awosborn <alanturing@example.coclear>
2021-01-19 10:07:36 -0700

    Add User and Image model test

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,6 @@
 class Image < ApplicationRecord
+  validates_presence_of :title,
+                        :artist,
+                        :user_id
+  belongs_to :user
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :title}
+    it { should validate_presence_of :artist}
+    it { should validate_presence_of :user_id}
+  end
+
+  describe 'relationships' do
+    it {should belong_to :user}
+  end
+
+  describe 'instance methods' do
+  end
 end
