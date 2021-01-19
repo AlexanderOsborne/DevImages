ed1496e9a4bc39f8bbd920c145d7ab1d18795e43
awosborn <alanturing@example.coclear>
2021-01-19 12:26:23 -0700

    Add Login feature

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   
   #Sessions
   get '/login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
+  post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 end
